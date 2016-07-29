bf35cb8c67e5f83ec09f66a437a9c9cac5cdee8d
Michael <michael.kushniriov@gmail.com>
2016-07-29 11:52:29 +0300

    working on examples

--- client/main/mainCtrl.js
+++ client/main/mainCtrl.js
@@ -4,6 +4,7 @@ function MainCtrl($scope, $stateParams) {
     $scope.who = 'carpar';
     var vm = this;
     $scope.signid = $stateParams.signid;
+
     vm.sometext = 'this is the text';
     if ($scope.signid === 'edmont001') {
         $scope.signComment = 'You can park here now for free';
