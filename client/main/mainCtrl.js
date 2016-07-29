73df40a5565183eb5449d7e7f175e7b8e3de6733
Michael <michael.kushniriov@gmail.com>
2016-07-29 11:40:22 +0300

    added 2 examples

--- client/main/mainCtrl.js
+++ client/main/mainCtrl.js
@@ -2,7 +2,15 @@ MainCtrl.$inject = ['$scope', '$stateParams'];
 
 function MainCtrl($scope, $stateParams) {
     $scope.who = 'carpar';
+    var vm = this;
     $scope.signid = $stateParams.signid;
+    vm.sometext = 'this is the text';
+    if ($scope.signid === 'edmont001') {
+        $scope.signComment = 'You can park here now for free';
+    } else if ($scope.signid === 'edmont002') {
+        $scope.signComment = 'Currently you cannot park';
+
+    }
 }
 
 export default MainCtrl;
